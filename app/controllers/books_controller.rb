2db2106f983320d81e8846b53674f03e8f6b129a
EC2 Default User <ec2-user@ip-172-31-34-42.ap-northeast-1.compute.internal>
2023-02-07 12:38:10 +0000

    [Finish] ファイルの設定

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,39 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path
+    else
+      render :index
+    end
+  end
+  
+  def index
+    @books = Book.page(params[:page])
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+  
+  private
+
+  def bookparams
+    params.require(:book).permit(:title, :body)
+  end
+end
