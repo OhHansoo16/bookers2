00361156847797e934ea9a0740fb13c8dfae4b55
OhHansoo16 <ec2-user@ip-172-31-27-64.ap-northeast-1.compute.internal>
2023-09-18 15:26:34 +0900

    add: .github

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,7 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
-  
+
   def create
     @book_new = Book.new(book_params)
     @book_new.user_id = current_user.id
@@ -15,7 +15,7 @@ class BooksController < ApplicationController
       render :index
     end
   end
-  
+
   def index
     @books = Book.all
     @book_new = Book.new
@@ -27,11 +27,11 @@ class BooksController < ApplicationController
     @book_comment = BookComment.new
     @book_new = Book.new
   end
-  
+
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
@@ -41,19 +41,19 @@ class BooksController < ApplicationController
       render :edit
     end
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to books_path
   end
-  
+
   private
 
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
   def is_matching_login_user
     @book = Book.find(params[:id])
     @user = @book.user
